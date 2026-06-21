630507f63fa1855dbcf4998cd4f304079263a56b
Tejas Gund <gundtejas8@gmail.com>
2026-06-21 19:05:37 +0530

    test

--- MetaNodes.py
+++ MetaNodes.py
@@ -1,6 +1,15 @@
 from log import get_logger
+
+
+#database connection
 try:
-    from dbconfig import get_connection
-except Exception as e:
     logger = get_logger("Database Connection")
+    logger.info("Connectiong Database")
+    from dbconfig import get_connection
+    logger.info("Successfully connected to Database")
+
+except Exception as e:
     logger.error("%s",e)
+
+
+
